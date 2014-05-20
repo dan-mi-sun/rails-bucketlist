55d41b236dc5ef727ca5b156f6d4c3be55464fca
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-20 14:40:44 +0100

    controller unti test passing

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,2 +1,14 @@
 class DestinationsController < ApplicationController
+
+  def create
+    @destination = Destination.new(allowed_params) 
+    @destination.save!
+    redirect_to destination_path(@destination)
+  end
+
+  private
+  def allowed_params
+    params.require(:destination).permit(:name)
+  end
+
 end
