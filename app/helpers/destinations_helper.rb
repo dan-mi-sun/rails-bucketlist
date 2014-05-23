c64f5b19cef106e2352453c5974088abb6894a39
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-23 17:50:28 +0100

    add to_do_items controller

--- app/helpers/destinations_helper.rb
+++ app/helpers/destinations_helper.rb
@@ -1,2 +1,7 @@
 module DestinationsHelper
+
+  def new_to_do_item
+    ToDoItem.new
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522160013) do
+ActiveRecord::Schema.define(version: 20140523130121) do
 
   create_table "destinations", force: true do |t|
     t.string   "name"
@@ -26,6 +26,9 @@ ActiveRecord::Schema.define(version: 20140522160013) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
+    t.string   "location"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
