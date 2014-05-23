7e4fd81432b010bbdaee5732293d766572e9e236
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-23 14:27:21 +0100

    add geocoder to todo model

--- app/models/to_do_item.rb
+++ app/models/to_do_item.rb
@@ -1,9 +1,13 @@
 class ToDoItem < ActiveRecord::Base
   validates :title, :presence => true
+  validates :location, :presence => true
 
   belongs_to :destination
+  
+  geocoded_by :geocode_input
+  after_validation :geocode 
 
   def geocode_input
-
+    "#{location}, #{destination.title}"
   end
 end
