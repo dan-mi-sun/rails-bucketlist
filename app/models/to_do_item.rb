26e75dc3d3812cd038237ccb837640901345ca2a
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-23 14:02:55 +0100

    add longitutde latitude to to do items

--- app/models/to_do_item.rb
+++ app/models/to_do_item.rb
@@ -2,4 +2,8 @@ class ToDoItem < ActiveRecord::Base
   validates :title, :presence => true
 
   belongs_to :destination
+
+  def geocode_input
+
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140523125027) do
+ActiveRecord::Schema.define(version: 20140523130121) do
 
   create_table "destinations", force: true do |t|
     t.string   "name"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20140523125027) do
     t.datetime "updated_at"
     t.string   "description"
     t.string   "location"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
