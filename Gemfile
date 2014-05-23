21227d1132bb1531deab81a67e7f56cc0513ccf9
Daniel Sun <dan.mi.sun@gmail.com>
2014-05-23 13:36:51 +0100

    add geocoder gem

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'spring', group: :development
+gem 'geocoder'
 
 group :development, :test do
 	gem 'rspec-rails', '~> 3.0.0.beta'
---
+++ features/step_definitions/add_to_do_item_steps.rb
@@ -0,0 +1,21 @@
+Given(/^an exciting destination called "(.*?)"$/) do |name|
+  @destination = Destination.create!( :name => name )
+end
+
+When(/^I select "(.*?)"$/) do |name|
+  click_button name 
+end
+
+When(/^I fill in the description of the todo item$/) do
+  @title = Faker::Company.catch_phrase
+  @description = Faker::Company.bs
+  fill_in "Title", :with => @title
+  fill_in "Description", :with => @description 
+end
+
+Then(/^I should see my new todo item listed as activity under "(.*?)"$/) do |name|
+  save_and_open_page
+  expect(page.has_content?(@title)).to be true
+  expect(page.has_content?(@description)).to be true
+end
+
